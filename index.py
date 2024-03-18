8a38c5a900931947a6176d65189b81bd7c37878f
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2024-03-19 02:25:38 +0500

    add update time

--- index.py
+++ index.py
@@ -3,22 +3,21 @@ import shutil
 import math
 import concurrent.futures
 import json
+from datetime import datetime, timezone
 
 SRC_DIR = "ogg"
 DST_DIR = "out"
-LARGE_FILESIZE = 25 * 1024 * 1024
 NOISE_FILES = ["noise", "whitenoise", "pcnoise", "background"]
 DB_FILENAME = "db.json"
 DEBUG_DB = False
 BASE_URL = "https://github.com/zeozeozeo/clickpack-db/raw/main/out/"
 DELETE_DUPLICATES = True
-large_files = []
 
 # load db.json if it exists
-db = {}
+db = {'updated_at_iso': '', 'updated_at_unix': 0, 'clickpacks': {}}
 if os.path.exists(os.path.join(DST_DIR, DB_FILENAME)):
     print(f"Loading `{DB_FILENAME}`...")
-    with open(os.path.join(DST_DIR, DB_FILENAME)) as f:
+    with open(os.path.join(DST_DIR, DB_FILENAME), 'r', encoding='utf-8') as f:
         db = json.load(f)
 print(f"Initial database consists of {len(db)} entries")
 
@@ -73,34 +72,32 @@ def zip_dir(dir_name):
             print(f"Clickpack `{dir_name}` has a noise file")
         shutil.make_archive(os.path.join(DST_DIR, dir_name), 'zip', dir_path)
         final_size = os.path.getsize(os.path.join(DST_DIR, dir_name + '.zip'))
-        if final_size > LARGE_FILESIZE:
-            large_files.append(dir_name)
 
         print(f"{dir_name}: {human_size(initial_size)} => {human_size(final_size)}, -{human_size(initial_size - final_size)}")
-        db[dir_name] = {"size": final_size, "uncompressed_size": initial_size, "has_noise": has_noise, "url": BASE_URL + dir_name + '.zip'}
+        db['clickpacks'][dir_name] = {"size": final_size, "uncompressed_size": initial_size, "has_noise": has_noise, "url": BASE_URL + dir_name + '.zip'}
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.map(zip_dir, os.listdir(SRC_DIR))
 
-print(f"\nFiles larger than {human_size(LARGE_FILESIZE)}:")
-for file in large_files:
-    print(file)
-else:
-    print("None!\n")
-
-print(f"Removed {len(dups)} duplicates in total: {', '.join(dups)}")
+print(f"\nRemoved {len(dups)} duplicates in total: {', '.join(dups)}")
 
 # sort database alphabetically (case-insensitive)
-db = {k: db[k] for k in sorted(db, key=str.lower)}
+db['clickpacks'] = {k: db['clickpacks'][k] for k in sorted(db['clickpacks'], key=str.lower)}
+
+# set current time
+now = datetime.now(timezone.utc)
+db['updated_at_iso'] = now.isoformat()
+db['updated_at_unix'] = int(round(now.timestamp()))
+print('Updated at: ' + db['updated_at_iso'])
 
 actual_filename = DB_FILENAME
 if DEBUG_DB:
     actual_filename = "debug_" + DB_FILENAME
-with open(os.path.join(actual_filename), "w") as f:
+with open(os.path.join(actual_filename), "w", encoding="utf-8") as f:
     if DEBUG_DB:
         json.dump(db, f, indent=4)
     else:
         json.dump(db, f, separators=(',', ':'))
 print(f"Final database consists of {len(db)} entries and is saved to `{actual_filename}`")
-total_size = sum(map(lambda x: x["size"], db.values()))
+total_size = sum(map(lambda x: x["size"], db['clickpacks'].values()))
 print(f"Total database size (compressed): {human_size(total_size)}")
